de2a33c24274ef91a842a197ef314e25b7a212f0
Andrew Plotkin <zarf@ZarfLent.local>
2026-04-16 21:10:22 -0400

    Static info.

--- src/custom/info.ts
+++ src/custom/info.ts
@@ -2,11 +2,20 @@
 /* Return the initial sourceloc to display. */
 export function sourceloc_start() : string
 {
-    return 'J:78:1:101:0';  // 'gverbs.zil', lines 78-100
+    return 'K:251:1:265:0';  // 'verbs.zil', lines 251-264
 }
 
 // Presentation order. Filenames must match game-info!
 export const sourcefile_presentation_list: string[] = [
     'sf.zil',
-    //...more files...
+    'dungeon.zil',
+    'emerg.zil',
+    'actions.zil',
+    'main.zil',
+    'parser.zil',
+    'syntax.zil',
+    'verbs.zil',
+    'macros.zil',
+    'clock.zil',
+    'crufty.zil',
 ];
